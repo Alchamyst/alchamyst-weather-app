dccf8d894cdeb0cd0adc2408a57ca945e8b5ee2a
Alchamyst <ashley@ashley-yetman.co.uk>
2023-11-18 17:15:05 +0000

    add weather api call to Weather component for Cheltenham.

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,16 +1,37 @@
 import { useState } from 'react';
-import { getCurrentWeather } from '../utils/weatherData';
 
 export default function Project(props) {
-    const [currentWeather, setcurrentWeather] = useState("");
-    const [errorMessage, seterrorMessage] = useState(undefined);
+    const [currentWeather, setCurrentWeather] = useState({});
+    const [errorMessage, setErrorMessage] = useState(undefined);
 
-    const fetchWeather = () => {
-        const fetchedWeather = getCurrentWeather("Cheltenham");
+    const handleOnClick = () => {
+        fetchCurrentWeather("Cheltenham");
+    }
 
-        // if (fetchedWeather.error) return seterrorMessage(fetchedWeather.error);
 
-        // setcurrentWeather(fetchedWeather);
+    const fetchCurrentWeather = (locationQuery) => {
+        const searchUrl = `/api/weather?address=${locationQuery}`;
+
+        return fetch(searchUrl)
+            .then((response) => response.json())
+            .then((weatherData) => parseWeather(weatherData));
+    }
+
+    const parseWeather = (weatherData) => {
+        console.log(weatherData);
+
+        if (weatherData.error) return setErrorMessage (weatherData.error);
+
+        setErrorMessage (undefined);
+        setCurrentWeather({
+            location: weatherData.location,
+            observationTime: weatherData.forecastData.observation_time,
+            feelsLike: weatherData.forecastData.feelslike, 
+            precip: weatherData.forecastData.precip,
+            temperature: weatherData.forecastData.temperature,
+            windSpeed: weatherData.forecastData.wind_speed,
+            weatherDescriptions: weatherData.forecastData.weather_descriptions,
+        })
     }
 
     return (
@@ -21,9 +42,15 @@ export default function Project(props) {
                 <button>Search</button>
             </form>
 
-            <button onClick={() => {fetchWeather()}}>Cheltenham Weather</button>
+            <button onClick={handleOnClick}>Cheltenham Weather</button>
             {errorMessage && <p>{errorMessage}</p>}
-            {!errorMessage && <p>{currentWeather}</p>}
+            {currentWeather.location && <>
+                <p>{currentWeather.location}</p>
+                <p>Observed at {currentWeather.observationTime}</p>
+                <p>Temperature: {currentWeather.temperature} &#176;C</p>
+                <p>Wind Speed: {currentWeather.windSpeed} kmph</p>
+                <p>Precipitation: {currentWeather.precip} mm</p>
+            </>}
         </>
     )
 }
