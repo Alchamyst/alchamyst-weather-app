773b3ba50b429d3a53cf0350b1df230c997f8a49
alchamyst <ashley@ashley-yetman.co.uk>
2023-11-21 01:20:17 +0000

    add footer link to alchamyst.com. update app styles.

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Header () {
     return (
         <>
             <footer className='footer bg-secondary text-light'>
-                <p>This is my footer component.</p>
+                <p className='footer-text'>Created by <a href="https://www.alchamyst.com">Alchamyst</a>.</p>
             </footer>
         </>
     );
